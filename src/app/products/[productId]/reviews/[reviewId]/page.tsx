0413bc271a89dae80fd8b33b1a55e3c9d7a8df61
yamazakiyoshiki <yoshiky1129@gmail.com>
2024-02-03 11:47:24 +0900

    Error Routing

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,7 +1,17 @@
 import { notFound } from 'next/navigation';
 import React from 'react'
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 const ReviewDetail = ({params}:{params: {productId: string, reviewId: string}}) => {
+  // const random = getRandomInt(2);
+
+  // if(random === 1) {
+  //   throw new Error("Error loading review");
+  // }
+
   if(parseInt(params.reviewId) > 1000) {
     notFound();
   };
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import Link from 'next/link'
 
 const ProductList = () => {
+  const productId = 100;
   return (
     <>
+    <Link href="/">Home</Link>
     <h1>ProductsList</h1>
-      <h2>Product1</h2>
-      <h2>Product2</h2>
-      <h2>Product3</h2>
+      <h2><Link href="products/1">Product1</Link></h2>
+      <h2><Link href="products/2">Product2</Link></h2>
+      <h2><Link href="products/3" replace>Product3</Link></h2>
+      <h2><Link href={`products/${productId}`}>Products {productId}</Link></h2>
     </>
   )
 }
